853272d23dc30c62c6b77b8d1bcd2540bae30133
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-22 15:18:11 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,17 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+    
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  
 end
 
