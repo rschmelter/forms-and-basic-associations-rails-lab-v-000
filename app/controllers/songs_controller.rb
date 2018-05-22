be5be84f33e1e164db43d97f9fec94787ca9508a
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-22 15:23:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist)
+    params.require(:song).permit(:title, :artist_name)
   end
 end
